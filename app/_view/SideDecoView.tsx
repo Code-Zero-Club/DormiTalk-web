473ea372e448f9cccc2679db62a5e026fd4d33f6
TaeHyun Kim <rlaxogus7094@naver.com>
2024-09-20 15:40:56 +0900

    feat : 기본 세팅

---
+++ app/_view/SideDecoView.tsx
@@ -0,0 +1,7 @@
+export default function SideDecoView() {
+  return (
+    <div className="w-3/5 h-screen bg-gray-950">
+      <h1 className="font-pretendard text-3xl font-bold text-white">개발 보류</h1>
+    </div>
+  );
+}
