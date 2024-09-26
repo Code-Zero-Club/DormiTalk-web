16abce5eacab1507dfbdb45e5be74b43e8d49712
DO_S <rlaxogus7094@naver.com>
2024-09-26 12:41:15 +0900

    feat : header 컴포넌트

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "./_components/Header";
+
 export default function Home() {
   return (
-    <main className="flex flex-row h-screen">
-      <div>asd</div>
+    <main className="flex flex-row h-screen bg-zinc-50 dark:bg-zinc-950 items-start">
+      <Header title="홈" />
     </main>
   );
 }
